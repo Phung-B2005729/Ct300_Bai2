d7dc53b5126f00905e068a98e34b433a0387682a
Phung-B2005729 <phungb2005729@student.ctu.edu.vn>
2023-02-21 08:39:22 +0700

    bai tap 2 cap nhat nhanh hotfix

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
         System.out.println("Toi la Phung");
+		System.out.println("DAY LA BAI THUC HANH GIT BUOI 2 CUA TOI");
 	}
 	}
